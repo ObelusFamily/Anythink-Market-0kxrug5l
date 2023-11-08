212ae700aff8544db7ce5c95ad7d5a3c60ea3191
Hasan Baygül <hasanbaygul@outlook.com.tr>
2023-11-08 00:16:40 +0000

    Update models and seeds to use Item instead of Product

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -4,17 +4,17 @@ const mongoose = require('mongoose');
 const faker = require('faker');
 
 const User = mongoose.model('User', new mongoose.Schema({ name: String, email: String }));
-const Product = mongoose.model('Product', new mongoose.Schema({ name: String, price: Number }));
-const Comment = mongoose.model('Comment', new mongoose.Schema({ user: String, product: String, text: String }));
+const Item = mongoose.model('Item', new mongoose.Schema({ name: String, price: Number }));
+const Comment = mongoose.model('Comment', new mongoose.Schema({ user: String, Item: String, text: String }));
 
 async function seedDB() {
     for(let i = 0; i < 100; i++) {
         const user = new User({ name: faker.name.findName(), email: faker.internet.email() });
-        const product = new Product({ name: faker.commerce.productName(), price: faker.commerce.price() });
-        const comment = new Comment({ user: user.name, product: product.name, text: faker.lorem.sentence() });
+        const item = new Item({ name: faker.commerce.itemName(), price: faker.commerce.price() });
+        const comment = new Comment({ user: user.name, item: item.name, text: faker.lorem.sentence() });
 
         await user.save();
-        await product.save();
+        await item.save();
         await comment.save();
     }
     mongoose.connection.close();
