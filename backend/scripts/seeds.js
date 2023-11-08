378e9fc810c782aed9500ebc591d4cf661c0c92c
Hasan Baygül <hasanbaygul@outlook.com.tr>
2023-11-08 00:25:37 +0000

    Update seed data with new models and fields

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -2,16 +2,17 @@
 
 const mongoose = require('mongoose');
 const faker = require('faker');
+const User = require('./models/User');
+const Item = require('./models/Item');
+const Comment = require('./models/Comment');
 
-const User = mongoose.model('User', new mongoose.Schema({ name: String, email: String }));
-const Item = mongoose.model('Item', new mongoose.Schema({ name: String, price: Number }));
-const Comment = mongoose.model('Comment', new mongoose.Schema({ user: String, Item: String, text: String }));
+mongoose.connect('mongodb://localhost:27017/myDatabase', {useNewUrlParser: true, useUnifiedTopology: true});
 
 async function seedDB() {
     for(let i = 0; i < 100; i++) {
         const user = new User({ name: faker.name.findName(), email: faker.internet.email() });
-        const item = new Item({ name: faker.commerce.itemName(), price: faker.commerce.price() });
-        const comment = new Comment({ user: user.name, item: item.name, text: faker.lorem.sentence() });
+        const item = new Item({ name: faker.commerce.productName(), price: faker.commerce.price() });
+        const comment = new Comment({ user: user._id, item: item._id, text: faker.lorem.sentence() });
 
         await user.save();
         await item.save();
